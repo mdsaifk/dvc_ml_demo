98612a03e19bb3df2674882de9f1d046106c2e35
mdsaifk <mdsaifkhan200@gmail.com>
2022-07-23 23:07:06 +0530

    satge_01created

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -1,9 +1,14 @@
 import yaml
 import os
 
-# cc = "dvc_ML//config//config.yaml"
+
 def read_yaml(path_of_yaml: str) -> dict:
     with open(path_of_yaml) as yaml_file:
-        content = yaml.safe_load_all(yaml_file)
+        content = yaml.safe_load(yaml_file)
     
     return content
+
+def create_directory(dirs: list):
+    for dir_path in dirs:
+        os.makedirs(dir_path,exist_ok =True)
+        print(f"directory created at {dir_path}")
---
+++ stage_01_load_save.py
@@ -0,0 +1,38 @@
+from fsutil import create_dir
+from src.utils.all_utils import read_yaml,create_directory
+import argparse
+import pandas as pd
+import os
+
+
+def get_data(config_path):
+    config = read_yaml(config_path)
+    url_of_data = config['data_source']
+    print(url_of_data)
+
+    df = pd.read_csv(url_of_data,sep=';')
+    print(df.head)
+
+# save the data in local directory
+
+    artifacts_dir = config['artifacts']['artifacts_dir']
+    raw_local_dir = config['artifacts']['raw_local_dir']
+    raw_local_file = config['artifacts']['raw_local_file']
+
+    raw_local_dir_path = os.path.join(artifacts_dir,raw_local_dir)
+    # print(raw_local_dir_path)
+    create_directory(dirs = [raw_local_dir_path])
+
+    raw_local_file_path = os.path.join(raw_local_dir_path,raw_local_file)
+    print(raw_local_file_path)
+
+    df.to_csv(raw_local_file_path,sep=',', index= False)
+    
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument("--config","-c",default= "config/config.yaml")
+    parsed_args = args.parse_args()
+    print(parsed_args.config)
+    get_data(config_path = parsed_args.config)
+
