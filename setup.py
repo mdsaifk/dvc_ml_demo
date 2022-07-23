a38fbd0b10772037e5c2266a0f39a0c31ee1ac5a
mdsaifk <mdsaifkhan200@gmail.com>
2022-07-23 16:24:30 +0530

    setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open("README.md","r",encoding = "utf-8") as f:
+     long_decription = f.read()
+
+setup(
+    name = "src",
+    version = "0.0.1",
+    author = "mdsaifk",
+    description = "A small package for dvc ML pipleine demo",
+    long_description= long_decription,
+    url = "https://github.com/mdsaifk/dvc_ml_demo",
+    author_email = "mdsaifkhan200@gmail.com",
+    package_name= ["src"],
+    license= "GNU",
+    python_requires = ">=3.7",
+    install_requires = ['dvc','pandas','scikit-learn']
+
+
+)
